fe2af8113ef8da7d2708eddbb7d9e68e2eba5117
Jake Stenger <jake@lindegroup.com>
2017-03-07 15:57:00 -0800

    function framework and variable declarations

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+var topics = [
+	"picard",
+	"data",
+	"geordi laforge",
+	"NCC-1701",
+	"riker",
+	"ferengi",
+	"janeway",
+	"romulans"
+];
+
+var opts = 
+	ajax : {
+		url : "",
+		method: "GET"
+	}
+}
