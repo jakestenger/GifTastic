ce6e90f7c0aaead3697e2ddd1c754762ac5b04f2
Jake Stenger <jake@lindegroup.com>
2017-03-08 17:01:34 -0800

    working api call function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,5 @@
+var api_key = "dc6zaTOxFJmzC";
+
 var topics = [
 	"picard",
 	"data",
@@ -9,9 +11,47 @@ var topics = [
 	"romulans"
 ];
 
-var opts = 
-	ajax : {
-		url : "",
+
+function giphy_search(query, num) {
+	console.log(num);
+	// replace all spaces with plus symbols using regex
+	query = query.replace(/ /g,"+");
+	// combine variables into a single url
+	queryURL = "http://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=" + api_key;
+	// ajax call
+	$.ajax({
+		url: queryURL,
 		method: "GET"
-	}
+	}).done(function(response) {
+		tablePlacement(num, response.data[num].images.fixed_width.url);
+	});
+	return;
 }
+
+function tablePlacement(num, url) {
+	var a = $("<img>");
+	a.attr("src", url);
+	a.addClass("gif");
+	$("#gif-grid").append(a);
+	return;
+}
+
+function animation(state){
+	if (state === 'on') {
+
+	} else if (state === 'off') {
+
+	} else {
+		console.log("unexpected argument");
+		return;
+	};
+}
+
+function getGifs(query, number) {
+	for (var i = 0; i <= number; i++) {
+		return giphy_search(query, i);
+	};
+}
+
+
+getGifs("picard", 20);
