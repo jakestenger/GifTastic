170035a332737dd758cb16b31cf944a8cd2e2e4b
Jake Stenger <jake@lindegroup.com>
2017-03-09 16:47:08 -0800

    click functions working, buttons exist

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,45 @@
-var api_key = "dc6zaTOxFJmzC";
+///////////////////////////////////////////////////////////////////////////////
+// VARIABLE DECLARATIONS
+///////////////////////////////////////////////////////////////////////////////
 
+var api_key = "dc6zaTOxFJmzC";
 var topics = [
-	"picard",
-	"data",
-	"geordi laforge",
+	"Picard",
+	"Data",
+	"Geordi laforge",
 	"NCC-1701",
-	"riker",
-	"ferengi",
-	"janeway",
-	"romulans"
+	"Riker",
+	"Ferengi",
+	"Janeway",
+	"Romulans"
 ];
+
+///////////////////////////////////////////////////////////////////////////////
+// PRIMARY AND UTILITY FUNCTIONS
+///////////////////////////////////////////////////////////////////////////////
 
 function buttoner() {
 	$("#button-row").empty();
 	for (var i = 0; i < topics.length; i++) {
-		b = $("<button>");
-		b.addClass("")
-		$("#button-row").append()
+		var b = $("<button>");
+		b.addClass("btn btn-info search");
+		b.attr("id", "btn-" + i);
+		b.attr("topic-index", i);
+		b.text(topics[i]);
+		$("#button-row").append(b);
 	}
+}
+
+function makeGrid() {
+
 }
 
 function giphy_search(query, num) {
 	// replace all spaces with plus symbols using regex
 	query = query.replace(/ /g,"+");
 	// combine variables into a single url
-	queryURL = "http://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=" + api_key;
+	queryURL = "http://api.giphy.com/v1/gifs/search?q=" + query + 
+		"&api_key=" + api_key;
 	// ajax call
 	$.ajax({
 		url: queryURL,
@@ -49,14 +64,37 @@ function animation(state){
 	} else {
 		console.log("unexpected argument");
 		return;
-	};
+	}
 }
 
 function getGifs(query, number) {
+	$("#gif-grid").empty();
 	for (var i = 0; i <= number; i++) {
 		giphy_search(query, i);
-	};
+	}
 }
 
+///////////////////////////////////////////////////////////////////////////////
+// CLICK FUNCTIONS BELOW
+///////////////////////////////////////////////////////////////////////////////
 
-getGifs("picard", 20);
+$(document).on("click", ".search", function(){
+	var id = $(this).attr("topic-index");
+	getGifs(topics[id], 20);
+});
+
+$("#btn-add").on("click", function(){
+	event.preventDefault();
+	topics.push($("#add-topic-text").val().trim());
+	$("#add-topic-text").val("");
+	buttoner();
+	getGifs(topics[topics.length - 1], 20);
+});
+
+///////////////////////////////////////////////////////////////////////////////
+// MAIN
+///////////////////////////////////////////////////////////////////////////////
+
+$(document).ready(function(){
+	buttoner();
+});
